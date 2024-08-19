f4a1e3cd8939918d5a36eec0ce02c9cbe709d907
RamonaSusanu <ramona.susanu03@gmail.com>
2024-08-19 09:01:49 -0700

    display tasks

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<h1>
+    The list of tasks
+</h1>
+
+<div>
+    @if(count($tasks)) 
+    <div> There are tasks!</div>   
+        
+    @else
+    <div> There are no tasks</div>
+        
+    @endif
+</div>
+
